2890e659457aae3a2c1ca6d60aa5df92587001ba
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-01-08 22:45:50 +0500

    Investigate virtual time in test

--- src/test/java/com/learnreactivespring/flaxandmonopalayground/FluxAndMonoCombineTest.java
+++ src/test/java/com/learnreactivespring/flaxandmonopalayground/FluxAndMonoCombineTest.java
@@ -3,6 +3,7 @@ package com.learnreactivespring.flaxandmonopalayground;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 
 import java.time.Duration;
 
@@ -49,15 +50,27 @@ public class FluxAndMonoCombineTest {
 
     @Test
     public void combineUsingConcat_withDelay() {
+
+        VirtualTimeScheduler.getOrSet();
+
         Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
         Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));
 
         Flux<String> mergedFlux = Flux.concat(flux1, flux2).log();
 
-        StepVerifier.create(mergedFlux)
+        StepVerifier.withVirtualTime(() -> mergedFlux)
                 .expectSubscription()
-                .expectNext("A", "B", "C", "D", "E", "F")
+                .thenAwait(Duration.ofSeconds(6))
+                .expectNextCount(6)
                 .verifyComplete();
+
+
+//        StepVerifier.create(mergedFlux)
+//                .expectSubscription()
+//                .expectNext("A", "B", "C", "D", "E", "F")
+//                .verifyComplete();
+
+
     }
 
     @Test
