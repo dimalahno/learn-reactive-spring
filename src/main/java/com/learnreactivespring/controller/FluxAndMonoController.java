75b66878ac934626505856217aff2e09b2022b6b
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-01-09 21:09:42 +0500

    Investigate flux controller.

---
+++ src/main/java/com/learnreactivespring/controller/FluxAndMonoController.java
@@ -0,0 +1,26 @@
+package com.learnreactivespring.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+import static org.springframework.http.MediaType.*;
+
+@RestController
+public class FluxAndMonoController {
+
+    @GetMapping("/flux")
+    public Flux<Integer> returnFlux() {
+        return Flux.just(1,2,3,4)
+                .log();
+    }
+
+    @GetMapping(value = "/fluxstream", produces = APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Long> returnFluxStream() {
+        return Flux.interval(Duration.ofSeconds(1)).log();
+    }
+}
