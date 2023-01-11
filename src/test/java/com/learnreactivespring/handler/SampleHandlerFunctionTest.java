c19ed8b4685806cd903f3efb95798fa8ab098716
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-01-11 23:25:00 +0500

    Write test for handler.

---
+++ src/test/java/com/learnreactivespring/handler/SampleHandlerFunctionTest.java
@@ -0,0 +1,53 @@
+package com.learnreactivespring.handler;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class SampleHandlerFunctionTest {
+
+    @Autowired
+    WebTestClient client;
+
+    @Test
+    public void flux_approach1() {
+        Flux<Integer> integerFlux = client.get()
+                .uri("/functional/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectSubscription()
+                .expectNext(1,2,3,4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void mono() {
+        var expected = Integer.valueOf(1);
+
+        client.get()
+                .uri("/functional/mono")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer.class)
+                .consumeWith(res -> assertEquals(expected, res.getResponseBody()));
+    }
+}
