c4e286525e527c11066586e00dd63ffa1fcce1d3
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-01-14 17:35:19 +0500

    Add test for get item by id

--- src/test/java/com/learnreactivespring/repository/ItemReactiveRepositoryTest.java
+++ src/test/java/com/learnreactivespring/repository/ItemReactiveRepositoryTest.java
@@ -28,7 +28,8 @@ public class ItemReactiveRepositoryTest {
     List<Item> itemList = Arrays.asList(new Item(null, "Samsung TV", 400.0),
             new Item(null, "LG TV", 420.0),
             new Item(null, "Apple Watch", 299.99),
-            new Item(null, "Beats Headphones", 149.99));
+            new Item(null, "Beats Headphones", 149.99),
+            new Item("ABC", "Bose Headphones", 149.99));
 
     @BeforeEach
     public void setUp() {
@@ -43,7 +44,23 @@ public class ItemReactiveRepositoryTest {
     public void getAllItems() {
         StepVerifier.create(repository.findAll())
                 .expectSubscription()
-                .expectNextCount(4)
+                .expectNextCount(5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void getItemById() {
+        StepVerifier.create(repository.findById("ABC"))
+                .expectSubscription()
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    public void getItemById1() {
+        StepVerifier.create(repository.findById("ABC"))
+                .expectSubscription()
+                .expectNextMatches(item -> item.getDescription().equals("Bose Headphones"))
                 .verifyComplete();
     }
 }
