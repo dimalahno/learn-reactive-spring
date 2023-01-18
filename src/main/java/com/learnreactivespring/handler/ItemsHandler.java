f4a488195b2a48d6ce8edd0eeefc9be934cc8b13
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-01-18 21:06:25 +0500

    Add handlers for getting, creating and deleting Item.

--- src/main/java/com/learnreactivespring/handler/ItemsHandler.java
+++ src/main/java/com/learnreactivespring/handler/ItemsHandler.java
@@ -2,14 +2,22 @@ package com.learnreactivespring.handler;
 
 import com.learnreactivespring.document.Item;
 import com.learnreactivespring.repository.ItemReactiveRepository;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+
+import static com.learnreactivespring.constants.ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+
 @Component
 public class ItemsHandler {
+
+    static Mono<ServerResponse> notFound = ServerResponse.notFound().build();
+    private final String ID = "id";
 
     final ItemReactiveRepository repository;
 
@@ -19,7 +27,29 @@ public class ItemsHandler {
 
     public Mono<ServerResponse> getAllItems(ServerRequest request) {
         return ServerResponse.ok()
-                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(APPLICATION_JSON)
                 .body(repository.findAll(), Item.class);
+    }
+
+    public Mono<ServerResponse> getOneItem(ServerRequest request) {
+        var id = request.pathVariable(ID);
+        Mono<Item> itemMono = repository.findById(id);
+        return itemMono.flatMap(item -> ServerResponse.ok().contentType(APPLICATION_JSON).body(fromValue(item)))
+                .switchIfEmpty(notFound);
+    }
+
+    public Mono<ServerResponse> createItem(ServerRequest request) {
+        Mono<Item> createdItem = request.bodyToMono(Item.class);
+        return createdItem.flatMap(item -> ServerResponse.created(URI.create(ITEM_FUNCTIONAL_END_POINT_V1))
+                .contentType(APPLICATION_JSON)
+                .body(repository.save(item), Item.class));
+    }
+
+    public Mono<ServerResponse> deleteItem(ServerRequest request) {
+        var id = request.pathVariable(ID);
+        Mono<Void> deletedItem = repository.deleteById(id);
+        return ServerResponse.ok()
+                .contentType(APPLICATION_JSON)
+                .body(deletedItem, Void.class);
     }
 }
--- src/main/java/com/learnreactivespring/router/ItemsRouter.java
+++ src/main/java/com/learnreactivespring/router/ItemsRouter.java
@@ -3,20 +3,26 @@ package com.learnreactivespring.router;
 import com.learnreactivespring.handler.ItemsHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static com.learnreactivespring.constants.ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1;
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration
 public class ItemsRouter {
 
     @Bean
     public RouterFunction<ServerResponse> itemsRoute(ItemsHandler handler){
-        return RouterFunctions.route(GET(ITEM_FUNCTIONAL_END_POINT_V1).and(accept(MediaType.APPLICATION_JSON)), handler::getAllItems);
+        return RouterFunctions.route(GET(ITEM_FUNCTIONAL_END_POINT_V1).and(accept(APPLICATION_JSON)),
+                        handler::getAllItems)
+                .andRoute(GET(ITEM_FUNCTIONAL_END_POINT_V1.concat("/{id}")).and(accept(APPLICATION_JSON)),
+                        handler::getOneItem)
+                .andRoute(POST(ITEM_FUNCTIONAL_END_POINT_V1).and(accept(APPLICATION_JSON)),
+                        handler::createItem)
+                .andRoute(DELETE(ITEM_FUNCTIONAL_END_POINT_V1.concat("/{id}")).and(accept(APPLICATION_JSON)),
+                        handler::deleteItem);
     }
 }
